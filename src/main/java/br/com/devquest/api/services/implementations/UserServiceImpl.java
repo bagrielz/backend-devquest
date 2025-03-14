c93c4016c8be5f184c32ec147ecd2690b905e7ea
Matheus <maatsimeao@gmail.com>
2025-03-14 14:29:10 -0300

    feat: Implementando método loadByUsername do UserServiceImpl

---
+++ src/main/java/br/com/devquest/api/services/implementations/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package br.com.devquest.api.services.implementations;
+
+import br.com.devquest.api.repositories.UserRepository;
+import br.com.devquest.api.services.IUserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserDetailsService, IUserService {
+
+  private UserRepository repository;
+
+  public UserServiceImpl(UserRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    var user = repository.findByUsername(username);
+    if (user != null) return user;
+    else throw new UsernameNotFoundException("Usuário " +username+ " não encontrado!");
+  }
+
+}
