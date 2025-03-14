c9dc7ae5fedcdeaa03889c3d7b67c45e6b0dc9f4
Matheus <maatsimeao@gmail.com>
2025-03-14 12:02:14 -0300

    chore: Criando mapeamento da entidade Permission

--- src/main/java/br/com/devquest/api/model/entities/Permission.java
+++ src/main/java/br/com/devquest/api/model/entities/Permission.java
@@ -1,4 +1,33 @@
 package br.com.devquest.api.model.entities;
 
-public class Permission {
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "permission")
+public class Permission implements GrantedAuthority, Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "description")
+  private String description;
+
+  @Override
+  public String getAuthority() {
+    return this.description;
+  }
+
 }
