ef3b6b9c3a3b5a2ddd8fef819212ee4b127e4621
Matheus <maatsimeao@gmail.com>
2025-03-14 14:46:08 -0300

    feat: Criando CustomizedExceptionHandler para capturar exceptions

---
+++ src/main/java/br/com/devquest/api/exceptions/handlers/CustomizedExceptionHandler.java
@@ -0,0 +1,35 @@
+package br.com.devquest.api.exceptions.handlers;
+
+import br.com.devquest.api.exceptions.response.ExceptionResponse;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@Hidden
+@ControllerAdvice
+@RestController
+public class CustomizedExceptionHandler {
+
+  @ExceptionHandler(UsernameNotFoundException.class)
+  public final ResponseEntity<ExceptionResponse> usernameNotFoundException(Exception ex, WebRequest request) {
+    ExceptionResponse exceptionResponse = createExceptionResponse(ex, request);
+    return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+  }
+
+  private static ExceptionResponse createExceptionResponse(Exception ex, WebRequest request) {
+    ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+            .timestamp(new Date())
+            .message(ex.getMessage())
+            .details(request.getDescription(false))
+            .build();
+    return exceptionResponse;
+  }
+
+}
