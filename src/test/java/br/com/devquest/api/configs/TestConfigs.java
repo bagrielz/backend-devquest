0d948e4e3e75bd0557f6c46518d5e6582d4a1314
Matheus <maatsimeao@gmail.com>
2025-03-16 20:59:12 -0300

    chore: Criando interface com constantes para testes

---
+++ src/test/java/br/com/devquest/api/configs/TestConfigs.java
@@ -0,0 +1,11 @@
+package br.com.devquest.api.configs;
+
+public interface TestConfigs {
+
+  int SERVER_PORT = 8888;
+  String HEADER_PARAM_AUTHORIZATION = "Authorization";
+  String HEADER_PARAM_ORIGIN = "Origin";
+
+  String ORIGIN_LOCAL = "http://localhost:8080";
+
+}
