33e7cc51a0b81400a25bc1353a293ea770ca677a
Matheus <maatsimeao@gmail.com>
2025-03-18 09:26:24 -0300

    chore: Iniciando mapeamento da entidade Question

---
+++ src/main/java/br/com/devquest/api/model/entities/Question.java
@@ -0,0 +1,35 @@
+package br.com.devquest.api.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "question")
+public class Question implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private Technology technology;
+  private Difficulty difficulty;
+  private String text;
+  private String correctAnswer;
+  private String justification;
+  private Date createdAt;
+  private List<QuestionOption> options;
+  private List<User> users;
+
+}
