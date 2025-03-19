7ef1e1ac6edfe81d1bdff5fd2dc6364ed6fe9e18
Matheus <maatsimeao@gmail.com>
2025-03-19 17:37:52 -0300

    feat: Criando endpoint getUserInfo de UserController

--- src/main/java/br/com/devquest/api/controllers/UserController.java
+++ src/main/java/br/com/devquest/api/controllers/UserController.java
@@ -1,6 +1,10 @@
 package br.com.devquest.api.controllers;
 
+import br.com.devquest.api.model.dtos.UserInfoDTO;
 import br.com.devquest.api.services.IUserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,11 @@ public class UserController {
 
   public UserController(IUserService service) {
     this.service = service;
+  }
+
+  @GetMapping("/getUserInfo")
+  public ResponseEntity<UserInfoDTO> getUserInfo(@RequestHeader("Authorization") String token) {
+    return service.getUserInfo(token);
   }
 
 }
