f67845505794e1e4fd1a3189db7ee058aaca8e0e
Matheus <maatsimeao@gmail.com>
2025-03-19 15:47:19 -0300

    test: Criando teste unitário do método getUserInfo para sucesso

--- src/test/java/br/com/devquest/api/unittests/mocks/MockUser.java
+++ src/test/java/br/com/devquest/api/unittests/mocks/MockUser.java
@@ -1,5 +1,6 @@
 package br.com.devquest.api.unittests.mocks;
 
+import br.com.devquest.api.model.entities.ActivityStatistics;
 import br.com.devquest.api.model.entities.Permission;
 import br.com.devquest.api.model.entities.User;
 import lombok.NoArgsConstructor;
@@ -26,6 +27,28 @@ public class MockUser {
             .credentialsNonExpired(true)
             .permissions(List.of(permission))
             .enabled(true)
+            .build();
+  }
+
+  public User mockUserWithActivityStatistics(Integer number) {
+    Long userId = Integer.toUnsignedLong(number);
+    ActivityStatistics activityStatistics = new MockActivityStatistics().mockActivityStatistics(number);
+    Permission permission = Permission.builder()
+            .id(1L)
+            .description("ADMIN")
+            .build();
+
+    return User.builder()
+            .id(userId)
+            .fullname("Fullname" +number)
+            .password("Password" +number)
+            .username("Username" +number)
+            .accountNonExpired(true)
+            .accountNonLocked(true)
+            .credentialsNonExpired(true)
+            .enabled(true)
+            .permissions(List.of(permission))
+            .activityStatistics(activityStatistics)
             .build();
   }
 
