f54fba9561a890ac5e1cc2d9fadacea59a714267
Matheus <maatsimeao@gmail.com>
2025-03-20 08:33:42 -0300

    docs: Documentando endpoint getUserInfo do UserController

--- src/main/java/br/com/devquest/api/controllers/UserController.java
+++ src/main/java/br/com/devquest/api/controllers/UserController.java
@@ -2,6 +2,8 @@ package br.com.devquest.api.controllers;
 
 import br.com.devquest.api.model.dtos.UserInfoDTO;
 import br.com.devquest.api.services.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "UserController", description = "Endpoints de manipulação de usuários")
 public class UserController {
 
   private IUserService service;
@@ -18,6 +21,7 @@ public class UserController {
     this.service = service;
   }
 
+  @Operation(summary = "Recuperar informações de usuários")
   @GetMapping("/getUserInfo")
   public ResponseEntity<UserInfoDTO> getUserInfo(@RequestHeader("Authorization") String token) {
     return service.getUserInfo(token);
