def797b3ca487067025ccde3dc57ccb0347b2c90
Matheus <maatsimeao@gmail.com>
2025-03-20 16:24:27 -0300

    feat: Criando endpoint generate de ExerciseController para gerar exercicio

--- src/main/java/br/com/devquest/api/controllers/ExerciseController.java
+++ src/main/java/br/com/devquest/api/controllers/ExerciseController.java
@@ -1,8 +1,12 @@
 package br.com.devquest.api.controllers;
 
+import br.com.devquest.api.enums.Difficulty;
+import br.com.devquest.api.enums.Technology;
+import br.com.devquest.api.model.dtos.ExerciseDTO;
 import br.com.devquest.api.services.IExerciseService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/exercises")
@@ -12,6 +16,15 @@ public class ExerciseController {
 
   public ExerciseController(IExerciseService service) {
     this.service = service;
+  }
+
+  @GetMapping("/generate")
+  public ResponseEntity<ExerciseDTO> generate(@RequestHeader("Authorization") String token,
+                                              @RequestParam("technology") Technology technology,
+                                              @RequestParam("difficulty") Difficulty difficulty) {
+
+    ExerciseDTO exerciseDTO = service.generateExercise(token, technology, difficulty);
+    return new ResponseEntity<>(exerciseDTO, HttpStatus.OK);
   }
 
 }
