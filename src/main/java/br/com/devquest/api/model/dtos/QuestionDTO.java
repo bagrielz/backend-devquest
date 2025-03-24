e68a9aaab3cc9927a5326aa092f7ad2007a728b9
Matheus <maatsimeao@gmail.com>
2025-03-24 10:02:42 -0300

    chore: Criando DTOs para auxiliar lógica do serviço generateQuestion

---
+++ src/main/java/br/com/devquest/api/model/dtos/QuestionDTO.java
@@ -0,0 +1,30 @@
+package br.com.devquest.api.model.dtos;
+
+import br.com.devquest.api.enums.Difficulty;
+import br.com.devquest.api.enums.Technology;
+import br.com.devquest.api.model.entities.QuestionOption;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QuestionDTO implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+  private Technology technology;
+  private Difficulty difficulty;
+  private String text;
+  private String correctAnswer;
+  private String justification;
+  private List<QuestionOptionDTO> questions;
+
+}
