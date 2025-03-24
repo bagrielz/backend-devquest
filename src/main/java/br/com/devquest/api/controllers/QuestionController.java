897911b29271fb4bb2a83a3af24282ba94599599
Matheus <maatsimeao@gmail.com>
2025-03-24 11:44:20 -0300

    feat: Criando endpoint generate de QuestionController

--- src/main/java/br/com/devquest/api/controllers/QuestionController.java
+++ src/main/java/br/com/devquest/api/controllers/QuestionController.java
@@ -1,8 +1,12 @@
 package br.com.devquest.api.controllers;
 
+import br.com.devquest.api.enums.Difficulty;
+import br.com.devquest.api.enums.Technology;
+import br.com.devquest.api.model.dtos.QuestionDTO;
 import br.com.devquest.api.services.IQuestionService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/questions")
@@ -12,6 +16,15 @@ public class QuestionController {
 
   public QuestionController(IQuestionService service) {
     this.service = service;
+  }
+
+  @GetMapping("/generate")
+  public ResponseEntity<QuestionDTO> generate(@RequestHeader("Authorization") String token,
+                                              @RequestParam("technology") Technology technology,
+                                              @RequestParam("difficulty") Difficulty difficulty) {
+
+    QuestionDTO questionDTO = service.generateQuestion(token, technology, difficulty);
+    return new ResponseEntity<>(questionDTO, HttpStatus.OK);
   }
 
 }
